825c69a125347bc693dab736fa9e8cf17c9363fb
marcus.oliveira <marcus.oliveira@WD19>
2023-04-05 16:30:00 -0400

    ajuste card

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{fontSize: "1.4em"}}>
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{fontSize: "1.25em"}}>
                                     Categoria
                                 </a>
                                 <ul className="dropdown-menu">
